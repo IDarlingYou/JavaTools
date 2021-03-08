58c2813216b6a00900916babbb756cec6ecb4208
尧 <y18736644276@163.com>
2021-03-08 16:20:57 +0800

    首次提交

---
+++ src/test/java/com/ly/tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
